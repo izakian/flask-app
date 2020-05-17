eaaa6d1008aae658aeff1404551c023376aa4101
Hesam Izakian <izakian@gmail.com>
2020-05-17 16:22:18 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from random import randint
+
+def rand(low_num, high_num):
+    """Generate rand number
+
+    Arguments:
+        low_num {int} -- int val
+        hight_num {int} -- int val
+
+    Returns:
+        int -- random in range
+    """
+    return randint(low_num, hight_num)
+
+ -
+  
+
+app=Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def health_check():
+    return "healthy!"
+
+@app.route('/rand_generate/<int:low>/<int:high>')
+def rand_generate(low, high):
+    return  str(randint(low, high))
+
+if __name__=='__main__':
+    app.run()
